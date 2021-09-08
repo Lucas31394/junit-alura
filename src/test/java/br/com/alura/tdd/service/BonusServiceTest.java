d18120f78541e9cf6907867e8f746df4ac5bc5e3
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-09-08 10:57:06 -0300

    Escrevendo os primeiros testes para a Classe BonusService

---
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,46 @@
+package br.com.alura.tdd.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
+class BonusServiceTest {
+
+	@Test
+	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Lucas", LocalDate.now(), new BigDecimal("25000")));
+		
+		assertEquals(new BigDecimal("0.00"), bonus);
+	}
+	
+	@Test
+	void bonusDeveriaSer10PorCentoDoSalario() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Lucas", LocalDate.now(), new BigDecimal("2500")));
+		
+		assertEquals(new BigDecimal("250.00"), bonus);
+	}
+	
+	@Test
+	void bonusDeveriaSerDe10PorCentoParaUmSalarioDeAte10000() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Lucas", LocalDate.now(), new BigDecimal("10000")));
+		
+		assertEquals(new BigDecimal("1000.00"), bonus);
+	}
+	
+	@Test
+	void bonusDeveSer0ParaSalarioAcimaDe10000() {
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Lucas", LocalDate.now(), new BigDecimal("10100")));
+		
+		assertEquals(new BigDecimal("0.00"), bonus);
+	}
+
+}
